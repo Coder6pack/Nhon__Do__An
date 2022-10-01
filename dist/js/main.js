f1ca2ec5ef67a49b495ca7b452e201e5ece4a3dc
Corder6pack <hoainhonlam10@gmal.com>
2022-10-01 15:51:21 +0700

    1/10/2022 1st

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,3 +4,41 @@ window.addEventListener("scroll", function () {
   let header = document.getElementsByClassName("header__main__nav")[0];
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+window.addEventListener("scroll", function () {
+  let header = document.getElementsByClassName("About__main__nav")[0];
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// slideshow
+
+let listImg = document.getElementsByClassName("slideshow__bg slide fade");
+let truocBtn = document.getElementsByClassName("truoc")[0];
+
+let index = 0;
+truocBtn.style.left = "0px";
+
+listImg[index].style.display = "inline";
+
+function hienThiSlideTruocDo() {
+  // [1,2,3,4] length = 4
+  if (index === 0) {
+    return (index = listImg.length - 1);
+  }
+  index -= 1;
+  setDisplay(index);
+}
+function hienThiSlideKeTiep() {
+  if (index === listImg.length - 1) {
+    return (index = 0);
+  }
+  index += 1;
+  setDisplay(index);
+}
+
+function setDisplay(index) {
+  for (let i = 0; i < listImg.length; ++i) {
+    listImg[i].style.display = "none";
+  }
+  listImg[index].style.display = "inline";
+}
